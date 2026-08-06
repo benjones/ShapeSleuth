c22b55c6f21a8f017abedce4b44f35ad7b9bf4a8
Ben Jones <ben.james.jones@gmail.com>
2026-08-05 21:37:43 -0600

    detect dragging in annulus

--- app/src/main/java/com/example/shapesleuth/composables/GuessWidget.kt
+++ app/src/main/java/com/example/shapesleuth/composables/GuessWidget.kt
@@ -1,5 +1,7 @@
 package com.example.shapesleuth.composables
 
+import android.util.Log
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
@@ -16,11 +18,16 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.center
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toOffset
 import com.example.shapesleuth.data.Card
 import com.example.shapesleuth.data.Colors
+import kotlin.math.abs
 import kotlin.math.cos
+import kotlin.math.min
 import kotlin.math.sin
 
 
@@ -35,27 +42,65 @@ import kotlin.math.sin
 fun GuesserWidget(modifier: Modifier = Modifier,
                   onSelect: (Card)-> Unit){
     var selectedColorIndex by remember{ mutableIntStateOf(0) }
+
+    var draggingColor by remember { mutableStateOf(false)}
+    var mouseX by remember { mutableStateOf(0f)}
+    var mouseY by remember { mutableStateOf(0f)}
+
+    var colorSelectionWidth = 5f
+
     val annulusCenterScale = 1/2.5f
     val annulusWidthScale = 1/10f
 
     Box(modifier
         //.background(Color.Black)
         .drawBehind({
-            drawCircle(brush = Brush.radialGradient(0.0f to Color.LightGray, 1.0f to Color.DarkGray),
-                style = Stroke(size.minDimension*annulusWidthScale),
-                radius= size.minDimension * annulusCenterScale
-                )
+            drawCircle(
+                brush = Brush.radialGradient(0.0f to Color.LightGray, 1.0f to Color.DarkGray),
+                style = Stroke(size.minDimension * annulusWidthScale),
+                radius = size.minDimension * annulusCenterScale
+            )
             Colors.entries.forEachIndexed { index, color ->
-                val angle = (2*Math.PI*index)/Colors.entries.size
-                drawCircle(brush = SolidColor(color.color),
+                val angle = (2 * Math.PI * index) / Colors.entries.size
+                drawCircle(
+                    brush = SolidColor(color.color),
                     center = Offset(
-                        (size.center.x + size.minDimension*annulusCenterScale*cos(angle)).toFloat(),
-                        (size.center.y + size.minDimension*annulusCenterScale* sin(angle)).toFloat()),
-                    radius = size.minDimension*annulusWidthScale/2
+                        (size.center.x + size.minDimension * annulusCenterScale * cos(angle)).toFloat(),
+                        (size.center.y + size.minDimension * annulusCenterScale * sin(angle)).toFloat()
+                    ),
+                    radius = size.minDimension * annulusWidthScale / 2
                 )
             }
 
-        })){
+            if(draggingColor){
+                drawCircle(
+                    brush=SolidColor(Color.Black),
+                    center = size.center, //Offset(mouseX, mouseY),
+                    radius = size.minDimension * annulusWidthScale / 2 + colorSelectionWidth/2,
+                    style = Stroke(colorSelectionWidth)
+                )
+            }
+
+        })
+        .pointerInput(Unit) {
+            detectDragGestures(
+                onDragStart = {offset -> //check if it's in the annulus
+                    Log.d("drag start", "offset: $offset")
+                    val center = size.center.toOffset()
+                    val len = (offset - center).getDistance()
+                    val minDim = min(size.width, size.height)
+                    val dist = abs(len - minDim*annulusCenterScale)
+                    if(dist  <= minDim*annulusWidthScale){
+                        draggingColor = true
+                    }
+                },
+                onDrag = { pointerInputChange, dragAmount -> },
+                onDragEnd = { draggingColor = false},
+                onDragCancel = {draggingColor = false}
+            )
+
+        }
+    ){
 
 
     }
